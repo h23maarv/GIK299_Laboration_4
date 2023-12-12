c7b2f2a4808e4c4b5b8057f735afd7b78ab7a495
h23maarv <h23maarv@du.se>
2023-12-12 13:19:20 +0100

    try/catch. Flyttat om i koden.

--- Arvidsson_Mattias_Labration4/Program.cs
+++ Arvidsson_Mattias_Labration4/Program.cs
@@ -19,7 +19,7 @@ namespace Arvidsson_Mattias_Labration4
                     "\nChoose one of the options: " +
                     "\n1.) Add person " +
                     "\n2.) Print out list of people " +
-                    "\n5.) Exit the program. " +
+                    "\n3.) Exit the program. " +
                     "\n------------------------------------------------"
                     );
 
@@ -30,7 +30,7 @@ namespace Arvidsson_Mattias_Labration4
                     case 1:
                         AddPerson(Personlist);
                         break;
-                        
+
                     case 2:
                         ListPersons(Personlist);
                         break;
@@ -38,49 +38,69 @@ namespace Arvidsson_Mattias_Labration4
                     case 3:
                         shouldExit = true;
                         break;
-                        
+
                     default:
                         Console.WriteLine("Invalid selection. Try again.");
                         break;
                 }
 
             }
+        }
 
         static void AddPerson(List<Person> personsList)
+        {
+            bool validInput = false;
+            while (!validInput)
             {
-                Console.WriteLine("\nEnter information about the person: ");
-
-                Console.WriteLine("Enter gender (0: Male, 1: Female, 2: Non-binary, 3: Other): ");
-                Gender gender = (Gender)Enum.Parse(typeof(Gender), Console.ReadLine());
-
-                Console.WriteLine("Enter hair length in cm: ");
-                int hairLength = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine("Enter hair color: ");
-                string hairColor = Console.ReadLine();
-
-                Hair hair = new Hair { HairLength = hairLength, HairColor = hairColor };
-
-                Console.WriteLine("Enter birthday (ÅÅÅÅ-MM-DD): ");
-                DateTime birthday = DateTime.Parse(Console.ReadLine());
-
-                Console.WriteLine("Enter eye color: ");
-                string eyeColor = Console.ReadLine();
-
-                Person newPerson = new Person(gender, hair, birthday, eyeColor);
-                personsList.Add(newPerson);
-
-                Console.WriteLine("Person added!");
-            }
-
-            static void ListPersons(List<Person> personsList) 
-            {
-                Console.WriteLine("\nList of people:");
-
-                foreach (var person  in personsList)
+                try
                 {
-                    Console.WriteLine(person + "\n");
+                    Console.WriteLine("\nEnter information about the person: ");
+
+                    Console.WriteLine("Gender (0: Male, 1: Female, 2: Non-binary, 3: Other): ");
+                    Gender gender = (Gender)Enum.Parse(typeof(Gender), Console.ReadLine());
+
+                    Console.WriteLine("Hair length in cm: ");
+                    int hairLength = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Hair color: ");
+                    string hairColor = Console.ReadLine();
+
+                    Hair hair = new Hair { HairLength = hairLength, HairColor = hairColor };
+
+                    Console.WriteLine("Birthday (ÅÅÅÅ-MM-DD): ");
+                    DateTime birthday = DateTime.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Eye color: ");
+                    string eyeColor = Console.ReadLine();
+
+                    Person newPerson = new Person(gender, hair, birthday, eyeColor);
+                    personsList.Add(newPerson);
+
+                    Console.WriteLine("Person added!");
+
+                    validInput = true;
                 }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Incorrect format. Please try again. ");
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine("Invalid input. Please try again. ");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Something went wrong. Please try again.");
+                }
+            }
+        }
+        static void ListPersons(List<Person> personsList)
+        {
+            Console.WriteLine("\nList of people:");
+
+            foreach (var person in personsList)
+            {
+                Console.WriteLine(person + "\n");
             }
         }
     }
--- Arvidsson_Mattias_Labration4/obj/Debug/net8.0/Arvidsson_Mattias_Labration4.AssemblyInfo.cs
+++ Arvidsson_Mattias_Labration4/obj/Debug/net8.0/Arvidsson_Mattias_Labration4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arvidsson_Mattias_Labration4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c7275df0abaace092e322742ef74c66afb9b896")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5886cd143d96f402989593e4a7fa0c174de1035")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arvidsson_Mattias_Labration4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arvidsson_Mattias_Labration4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
