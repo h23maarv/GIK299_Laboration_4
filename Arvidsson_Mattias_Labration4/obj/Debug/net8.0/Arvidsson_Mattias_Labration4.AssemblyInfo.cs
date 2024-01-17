b523781d66db21112a29ae1ce492bbc035312134
h23maarv <h23maarv@du.se>
2024-01-17 12:22:26 +0100

    .

--- Arvidsson_Mattias_Labration4/obj/Debug/net8.0/Arvidsson_Mattias_Labration4.AssemblyInfo.cs
+++ Arvidsson_Mattias_Labration4/obj/Debug/net8.0/Arvidsson_Mattias_Labration4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arvidsson_Mattias_Labration4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55399fd4c04b83c14c1da7dbcd9a5fcfa05d1c0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arvidsson_Mattias_Labration4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arvidsson_Mattias_Labration4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
